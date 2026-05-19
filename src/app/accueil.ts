a1149d1ae55a93f45f7b4910931c7d3df6aecfc5
NilsGouraud <gouraud.nils@gmail.com>
2026-05-19 18:39:45 +0200

    removed unused lifecycle hooks and event handlers in accueil.ts

--- src/app/accueil.ts
+++ src/app/accueil.ts
@@ -1,35 +1,9 @@
-import { ChangeDetectorRef, Component, OnChanges, SimpleChanges } from '@angular/core';
-import { backEndUrl } from './app';
+import { Component } from '@angular/core';
 @Component({
   selector: 'accueil',
   templateUrl: './accueil.html',
   styleUrl: './app.css',
 })
-export class Accueil implements OnChanges {
-  text = 'placeholder';
-  constructor(private cd: ChangeDetectorRef) {}
-  async ngOnInit() {
-    console.log('initializing home component');
-    let buffer = await this.getData();
-    console.log('the text has been changed');
-    this.text = buffer;
-    this.cd.detectChanges();
-  }
-
-  async getData() {
-    let request = backEndUrl;
-    let response = await fetch(request);
-    let data = await response.text();
-    return data;
-  }
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes) {
-      console.log('it changed');
-    }
-  }
-  async handle() {
-    console.log('event right there');
-    let buffer = await this.getData();
-    this.text = buffer;
-  }
+export class Accueil {
+  async ngOnInit() {}
 }
