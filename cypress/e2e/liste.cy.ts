5833f68fdf03200e24047ca8128eb2472837a4e3
NilsGouraud <gouraud.nils@gmail.com>
2026-05-28 14:46:22 +0200

    added data-cys to better identify elements in cypress

--- cypress/e2e/liste.cy.ts
+++ cypress/e2e/liste.cy.ts
@@ -1,16 +1,21 @@
-describe('main routes', () => {
+describe('liste', () => {
   it('should load page at /liste', () => {
     cy.visit('/liste');
     cy.contains(
       'Voici notre catalogue de cartes. Vous pouvez librement éditer chaque carte en cliquant dessus.',
     );
   });
+  beforeEach(() => {
+    cy.visit('/liste');
+  });
+
   it('should open a modal with a form when an article is clicked', () => {
     cy.get('article').then((articles) => {
       if (!articles.length) {
         return;
       }
       cy.get('article').eq(0).click();
+      cy.get('[data-cy="cardInForm"]').should('exist');
     });
   });
 });
