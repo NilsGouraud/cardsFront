0990f4548eaaf61c2def5975b6920f0aaf146897
NilsGouraud <gouraud.nils@gmail.com>
2026-06-07 15:44:22 +0200

    uncommented passed tests

--- cypress/e2e/listeAndAjouter.cy.ts
+++ cypress/e2e/listeAndAjouter.cy.ts
@@ -46,40 +46,40 @@ describe('liste', () => {
     cy.intercept('/liste').as('liste');
     cy.intercept('/ajouter').as('ajouter');
   });
-  // it('should load page at /liste', () => {
-  //   cy.visit('/liste');
-  //   cy.contains(
-  //     'Voici notre catalogue de cartes. Vous pouvez librement éditer chaque carte en cliquant dessus.',
-  //   );
-  // });
+  it('should load page at /liste', () => {
+    cy.visit('/liste');
+    cy.contains(
+      'Voici notre catalogue de cartes. Vous pouvez librement éditer chaque carte en cliquant dessus.',
+    );
+  });
 
-  // describe('formModal', () => {
-  //   beforeEach(() => {
-  //     cy.visit('/liste');
-  //     cy.get('article').then((articles) => {
-  //       if (!articles.length) {
-  //         //test is relevant without any data
-  //         return;
-  //       }
-  //       cy.get('article').eq(0).click();
-  //     });
-  //   });
-  //   it('should open a modal with a form when an article is clicked', () => {
-  //     cy.get('[data-cy="cardInForm"]').should('exist');
-  //   });
-  //   it('should close the modal when cancel is clicked', () => {
-  //     cy.get('[data-cy="cancelButton"]').click();
-  //     cy.get('[data-cy="cardInForm"]').should('not.exist');
-  //   });
-  //   it('should close the modal when escape is pressed', () => {
-  //     cy.get('body').type('{esc}');
-  //     cy.get('[data-cy="cardInForm"]').should('not.exist');
-  //   });
-  //   it('should close the modal when enter is pressed', () => {
-  //     cy.get('body').type('{enter}');
-  //     cy.get('[data-cy="cardInForm"]').should('not.exist');
-  //   });
-  // });
+  describe('formModal', () => {
+    beforeEach(() => {
+      cy.visit('/liste');
+      cy.get('article').then((articles) => {
+        if (!articles.length) {
+          //test is relevant without any data
+          return;
+        }
+        cy.get('article').eq(0).click();
+      });
+    });
+    it('should open a modal with a form when an article is clicked', () => {
+      cy.get('[data-cy="cardInForm"]').should('exist');
+    });
+    it('should close the modal when cancel is clicked', () => {
+      cy.get('[data-cy="cancelButton"]').click();
+      cy.get('[data-cy="cardInForm"]').should('not.exist');
+    });
+    it('should close the modal when escape is pressed', () => {
+      cy.get('body').type('{esc}');
+      cy.get('[data-cy="cardInForm"]').should('not.exist');
+    });
+    it('should close the modal when enter is pressed', () => {
+      cy.get('body').type('{enter}');
+      cy.get('[data-cy="cardInForm"]').should('not.exist');
+    });
+  });
 
   describe('create card', () => {
     it('should open a modal if a name is already taken, and the modal should disappear when clicked', () => {
@@ -104,6 +104,8 @@ describe('liste', () => {
       cy.get('#modalBackdrop').should('not.exist');
 
       deleteTestCard();
+      cy.visit('/liste');
+      cy.get('[data-cy="nom"]').last().should('not.have.text', globalName);
     });
     it('should not be possible to create from scratch instants and rituals with atk/pdv', () => {
       cy.visit('/ajouter');
@@ -114,7 +116,7 @@ describe('liste', () => {
       cy.get('#type').select('éphémère');
       cy.get('#type').should('have.value', 'éphémère');
     });
-    it('should alter fields', () => {
+    it('should be possible to cancel changes', () => {
       //TODO change field, save, check
       cy.visit('/ajouter');
       cy.get('#nom').type(globalName);
@@ -168,7 +170,69 @@ describe('liste', () => {
       cy.get('#inputAtk').should('have.value', globalAtk);
       cy.get('#inputCout').should('have.value', globalCout);
 
+      /*delete for further tests*/
       deleteTestCard();
+      cy.visit('/liste');
+      cy.get('[data-cy="nom"]').last().should('not.have.text', globalName);
+    });
+    it('should be possible to alter card values', () => {
+      //TODO change field, save, check
+      cy.visit('/ajouter');
+      cy.get('#nom').type(globalName);
+      cy.get('#nom').should('have.value', globalName);
+      cy.get('#effet').type(globalEffect);
+      cy.get('#effet').should('have.value', globalEffect);
+      cy.get('#description').type(globalDescription);
+      cy.get('#description').should('have.value', globalDescription);
+      cy.get('#type').select(globalType);
+      cy.get('#type').should('have.value', globalType);
+      cy.get('#atk').type(globalAtk);
+      cy.get('#atk').should('have.value', globalAtk);
+      cy.get('#pdv').type(globalPdv);
+      cy.get('#pdv').should('have.value', globalPdv);
+      cy.get('#cout').type(globalCout);
+      cy.get('#cout').should('have.value', globalCout);
+      cy.get('button').click();
+      cy.visit('/liste');
+      const alteredName = 'cyTestName';
+      const alteredEffect = 'mmm';
+      const alteredDescription = 'mmm';
+      const alteredType = 'éphémère';
+      const alteredCout = '0';
+      cy.get('[data-cy="nom"]').last().should('have.text', alteredName);
+
+      cy.visit('/ajouter');
+
+      cy.get('#nom').type(alteredName);
+      cy.get('#nom').should('have.value', alteredName);
+      cy.get('#effet').type(alteredEffect);
+      cy.get('#effet').should('have.value', alteredEffect);
+      cy.get('#description').type(alteredDescription);
+      cy.get('#description').should('have.value', alteredDescription);
+      cy.get('#type').select(alteredType);
+      cy.get('#type').should('have.value', alteredType);
+      cy.get('#atk').should('not.exist');
+      cy.get('#pdv').should('not.exist');
+      cy.get('#cout').type(alteredCout);
+      cy.get('#cout').should('have.value', alteredCout);
+
+      cy.get('button').click();
+      cy.get('#confirmButton').click();
+      cy.visit('/liste');
+      cy.get('article').last().click();
+
+      cy.get('#inputNom').should('have.value', alteredName);
+      cy.get('#inputEffet').should('have.value', alteredEffect);
+      cy.get('#inputDescription').should('have.value', alteredDescription);
+      cy.get('#inputType').find('option:selected').should('have.value', alteredType);
+      // cy.get('#inputPdv').should('have.value', alteredPdv);
+      // cy.get('#inputAtk').should('have.value', alteredAtk);
+      cy.get('#inputCout').should('have.value', alteredCout);
+
+      /*delete for further tests*/
+      deleteTestCard();
+      cy.visit('/liste');
+      cy.get('[data-cy="nom"]').last().should('not.have.text', globalName);
     });
   });
 });
