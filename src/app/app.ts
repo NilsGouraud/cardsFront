7054b10aee4710c38b20e8d631134190c2008a0a
NilsGouraud <gouraud.nils@gmail.com>
2026-06-10 22:01:27 +0200

    changed backendUrl for deployment

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,4 +8,9 @@ import { RouterOutlet, RouterLink } from '@angular/router';
   styleUrl: './app.css',
 })
 export class App {}
+
+/*for dev:
 export const backEndUrl = 'http://localhost:8080/';
+
+*/
+export const backEndUrl = 'https://cardsback-young-bush-4086.fly.dev/';
